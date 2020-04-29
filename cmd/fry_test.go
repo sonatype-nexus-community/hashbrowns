3b6750921f99a8392da7ea5921183385c29ab689
Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
2020-04-29 15:36:52 -0800

    End to end test, no error returned

--- cmd/fry_test.go
+++ cmd/fry_test.go
@@ -18,9 +18,39 @@ package cmd
 import (
 	"testing"
 
+	"github.com/jarcoal/httpmock"
 	"github.com/sonatype-nexus-community/hashbrowns/types"
 	"github.com/stretchr/testify/assert"
 )
+
+const applicationsResponse = `{
+	"applications": [
+		{
+			"id": "4bb67dcfc86344e3a483832f8c496419",
+			"publicId": "testapp",
+			"name": "TestApp",
+			"organizationId": "bb41817bd3e2403a8a52fe8bcd8fe25a",
+			"contactUserName": "NewAppContact",
+			"applicationTags": [
+				{
+					"id": "9beee80c6fc148dfa51e8b0359ee4d4e",
+					"tagId": "cfea8fa79df64283bd64e5b6b624ba48",
+					"applicationId": "4bb67dcfc86344e3a483832f8c496419"
+				}
+			]
+		}
+	]
+}`
+
+const thirdPartyAPIResultJSON = `{
+		"statusUrl": "api/v2/scan/applications/4bb67dcfc86344e3a483832f8c496419/status/9cee2b6366fc4d328edc318eae46b2cb"
+}`
+
+const pollingResult = `{
+	"policyAction": "None",
+	"reportHtmlUrl": "http://sillyplace.com:8090/ui/links/application/test-app/report/95c4c14e",
+	"isError": false
+}`
 
 func validateConfigFryError(t *testing.T, expectedErrorMsgSnippet string, expectedConfig types.Config, args ...string) {
 	_, err := executeCommand(rootCmd, args...)
@@ -49,4 +79,21 @@ func TestFryCommandConfigNoServerRunning(t *testing.T) {
 		types.Config{User: "admin", Token: "admin123", Server: "http://localhost:8070", Stage: "develop", MaxRetries: 300,
 			Path: "testdata/emptyFile", Application: "testapp"},
 		"fry", "--path=testdata/emptyFile", "--application=testapp")
+}
+
+func TestFryCommandWithRunningIQ(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder("GET", "http://sillyplace.com:8090/api/v2/applications?publicId=testapp",
+		httpmock.NewStringResponder(200, applicationsResponse))
+
+	httpmock.RegisterResponder("POST", "http://sillyplace.com:8090/api/v2/scan/applications/4bb67dcfc86344e3a483832f8c496419/sources/nancy?stageId=develop",
+		httpmock.NewStringResponder(202, thirdPartyAPIResultJSON))
+
+	httpmock.RegisterResponder("GET", "http://sillyplace.com:8090/api/v2/scan/applications/4bb67dcfc86344e3a483832f8c496419/status/9cee2b6366fc4d328edc318eae46b2cb",
+		httpmock.NewStringResponder(200, pollingResult))
+
+	_, err := executeCommand(rootCmd, "fry", "--path=testdata/emptyFile", "--application=testapp", "--server-url=http://sillyplace.com:8090")
+	assert.Nil(t, err)
 }
--- go.mod
+++ go.mod
@@ -10,5 +10,6 @@ require (
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/pflag v1.0.3
 	github.com/spf13/viper v1.6.3
+	github.com/jarcoal/httpmock v1.0.4
 	github.com/stretchr/testify v1.3.0
 )
